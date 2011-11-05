818af01e98b401a7dedad83ee9ad65b89e52e727
erik 'quantitative warlock' shilts <erik.shilts@opower.com>
2011-11-05 12:20:32 -0400

    vectorized gradient calcs

--- mlclass-ex3/mlclass-ex3/lrCostFunction.m
+++ mlclass-ex3/mlclass-ex3/lrCostFunction.m
@@ -38,10 +38,7 @@ grad = zeros(size(theta));
 
 J = (1 / m) * sum((-y .* log(sigmoid(X * theta))) - ((1 - y) .* log(1 - sigmoid(X * theta))));
 
-for ii = 1:size(theta, 1)
-    grad(ii) = (1 / m) * sum((sigmoid(X * theta) - y) .* X(:, ii));
-end
-
+grad = (1 / m) * X' * (sigmoid(X * theta) - y)
 
 % =============================================================
 
